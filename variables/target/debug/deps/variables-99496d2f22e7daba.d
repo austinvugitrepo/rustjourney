1f6ff8d32c27411b0472d837c5b440662fec1839
Austin <freeluck.v.i.p999@gmail.com>
2025-08-13 00:30:02 -0500

    went through chapter 3 and plan on going to chapter 2 when i finish this chapter, finished Variables and mutabilty portion of this chapter

---
+++ variables/target/debug/deps/variables-99496d2f22e7daba.d
@@ -0,0 +1,5 @@
+/home/austin/code-repos/rustjourney/variables/target/debug/deps/variables-99496d2f22e7daba.d: src/main.rs
+
+/home/austin/code-repos/rustjourney/variables/target/debug/deps/variables-99496d2f22e7daba: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/austin/code-repos/rustjourney/variables/target/debug/variables: /home/austin/code-repos/rustjourney/variables/src/main.rs
